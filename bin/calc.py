ebec3248705c439a968a63c97c4a0ba199d0d189
nariyuri <nariyuri@gmail.com>
2021-10-30 06:01:11 +0900

    Change calc.py

--- bin/calc.py
+++ bin/calc.py
@@ -2,5 +2,5 @@
 
 class EfficiencyCalc:
     def __init__(self, _atc, _def, _boss):
-        print(list(map(lambda x, y, z: 100*(((100+x)/100)*((100+y)/100))*(z/100*300-300+100), _atc, _def, _boss)))
+        list(map(lambda x, y, z: 100*(((100+x)/100)*((100+y)/100))*(z/100*300-300+100), _atc, _def, _boss))
     
